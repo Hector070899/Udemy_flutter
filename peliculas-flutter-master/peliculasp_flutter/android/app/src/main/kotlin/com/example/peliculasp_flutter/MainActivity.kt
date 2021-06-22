797172730f23e1875d40b233c0a1bfdf16ece547
Hector Morales <tuntu99@gmail.com>
2021-06-22 09:29:58 -0600

    Ahorita el proyecto no corre

---
+++ peliculas-flutter-master/peliculasp_flutter/android/app/src/main/kotlin/com/example/peliculasp_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.peliculasp_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- peliculas-flutter-master/peliculasp_flutter/ios/Flutter/flutter_export_environment.sh
+++ peliculas-flutter-master/peliculasp_flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\hecto\Documents\Udemy\Udemy_flutter\peliculas-flutter-master\peliculas-flutter-master"
+export "FLUTTER_APPLICATION_PATH=C:\Users\hecto\Documents\Udemy\Udemy_flutter\peliculas-flutter-master\peliculasp_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
